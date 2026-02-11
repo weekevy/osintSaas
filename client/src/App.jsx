29a2b5176e49d027cc069ea6a6ef664bc385bd5f
dvsys <dvsys@localhost.localdomain>
2026-02-11 11:58:07 +0100

    Update the new file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
 
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login.jsx";
 import Home from "./components/Home.jsx";
+import Welcome from "./components/Welcom.jsx"; // correct spelling
 import "./index.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Welcome/>} />
+        <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
