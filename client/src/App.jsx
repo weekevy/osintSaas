9b1d6606b71aa5eb2629ee6c4bc6e91dd0cd7ac2
dvsys <dvsys@localhost.localdomain>
2026-02-12 15:27:28 +0100

    Update: fix some routing in main file
    beset practice in this situation make to sub componenets contain it self for another components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/Login.jsx";
 import Home from "./components/Home.jsx";
 import Welcome from "./components/Welcom.jsx"; // correct spelling
 import "./index.css";
@@ -10,7 +9,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Welcome/>} />
-        <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
