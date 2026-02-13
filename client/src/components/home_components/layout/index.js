0108121cb89b000a53981b52a2455b5aa2e1d660
dvsys <dvsys@localhost.localdomain>
2026-02-13 16:17:04 +0100

    clien(Home) Dashboard add index.js

---
+++ client/src/components/home_components/layout/index.js
@@ -0,0 +1,4 @@
+export { default as Sidebar } from './Sidebar';
+export { default as MobileSidebar } from './MobileSidebar';
+export { default as TopBar } from './TopBar';
+export { default as UserMenu } from './UserMenu';
