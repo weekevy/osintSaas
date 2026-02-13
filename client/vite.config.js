584131bdb300b4ddc9f2299df41199bad2a860dc
dvsys <dvsys@localhost.localdomain>
2026-02-13 10:32:42 +0100

    config(client): add Vite proxy for API requests to port 4000

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    port:5173,
     proxy: {
       '/api': {
       target: 'http://localhost:4000', // your Next.js backend URL
