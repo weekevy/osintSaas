619c11b1b638f22dbd6a3ae552721361d7a668b2
dvsys <dvsys@localhost.localdomain>
2026-02-13 16:27:10 +0100

    client(/home): add index.js, relation between components and Home.jsx

---
+++ client/src/components/home_components/index.js
@@ -0,0 +1,23 @@
+// Layout
+export { default as Sidebar } from './layout/Sidebar';
+export { default as MobileSidebar } from './layout/MobileSidebar';
+export { default as TopBar } from './layout/TopBar';
+export { default as UserMenu } from './layout/UserMenu';
+
+// Dashboard
+export { default as DashboardHome } from './dashboard/DashboardHome';
+export { default as AnalysisDashboard } from './dashboard/AnalysisDashboard';
+export { default as ProjectsDashboard } from './dashboard/ProjectsDashboard';
+export { default as ReportsDashboard } from './dashboard/ReportsDashboard';
+export { default as TeamDashboard } from './dashboard/TeamDashboard';
+export { default as IntegrationsDashboard } from './dashboard/IntegrationsDashboard';
+export { default as APIsDashboard } from './dashboard/APIsDashboard';
+export { default as AnalyticsDashboard } from './dashboard/AnalyticsDashboard';
+
+// Risk components (if needed directly)
+export { default as RiskCircle } from './dashboard/RiskCircle';
+export { default as ThreatCurve } from './dashboard/ThreatCurve';
+export { default as AlertsSection } from './dashboard/AlertsSection';
+export { default as RecentScans } from './dashboard/RecentScans';
+export { default as ThreatFeed } from './dashboard/ThreatFeed';
+export { default as QuickTools } from './dashboard/QuickTools';
